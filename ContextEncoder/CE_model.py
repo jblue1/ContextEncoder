61186c64fb3f17816291242b8670f972b26f9e63
jblue1 <joblue@davidson.edu>
2019-07-03 11:44:35 -0400

    Changed model to use 128x175 images

--- ContextEncoder/CE_model.py
+++ ContextEncoder/CE_model.py
@@ -4,7 +4,7 @@ Functions to build the autoencoder/generator and discriminator as a keras model 
 import tensorflow as tf
 
 
-def build_autoencoder(use_gpu, channels=3, height=128, width=128,):
+def build_autoencoder(use_gpu, channels=1, height=128, width=175,):
     if use_gpu:
         data_format = 'channels_first'
         axis = 1
@@ -19,73 +19,56 @@ def build_autoencoder(use_gpu, channels=3, height=128, width=128,):
                                strides=2,
                                padding='same',
                                data_format=data_format)(encoder_input)
-
+    x = tf.keras.layers.BatchNormalization(axis)(x)
+    x = tf.keras.layers.LeakyReLU(0.2)(x)
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=4, strides=2, data_format=data_format)(x)
+    x = tf.keras.layers.BatchNormalization(axis)(x)
+    x = tf.keras.layers.LeakyReLU(0.2)(x)
+    x = tf.keras.layers.Conv2D(filters=256, kernel_size=4, strides=2, data_format=data_format)(x)
+    x = tf.keras.layers.BatchNormalization(axis)(x)
+    x = tf.keras.layers.LeakyReLU(0.2)(x)
+    x = tf.keras.layers.Conv2D(filters=256, kernel_size=4, strides=2, data_format=data_format)(x)
+    x = tf.keras.layers.BatchNormalization(axis)(x)
+    x = tf.keras.layers.LeakyReLU(0.2)(x)
+    x = tf.keras.layers.Conv2D(filters=512, kernel_size=4, strides=2, data_format=data_format)(x)
     x = tf.keras.layers.BatchNormalization(axis)(x)
     x = tf.keras.layers.LeakyReLU(0.2)(x)
 
-    x = tf.keras.layers.Conv2D(filters=64, kernel_size=4, strides=2, padding='same', data_format=data_format)(x)
-    x = tf.keras.layers.BatchNormalization(axis)(x)
-    x = tf.keras.layers.LeakyReLU(0.2)(x)
-    x = tf.keras.layers.Conv2D(filters=128, kernel_size=4, strides=2, padding='same', data_format=data_format)(x)
-    x = tf.keras.layers.BatchNormalization(axis)(x)
-    x = tf.keras.layers.LeakyReLU(0.2)(x)
-    x = tf.keras.layers.Conv2D(filters=256, kernel_size=4, strides=2, padding='same', data_format=data_format)(x)
-    x = tf.keras.layers.BatchNormalization(axis)(x)
-    # when used initializations of the center from the paper, started getting nans as the output of the layer below
-    x = tf.keras.layers.LeakyReLU(0.2)(x)
-    x = tf.keras.layers.Conv2D(filters=512, kernel_size=4, strides=2, padding='same', data_format=data_format)(x)
-    x = tf.keras.layers.BatchNormalization(axis)(x)
-    x = tf.keras.layers.LeakyReLU(0.2)(x)
-    x = tf.keras.layers.Conv2D(filters=1000, kernel_size=4, data_format=data_format)(x)
-
+    x = tf.keras.layers.Conv2D(filters=1000, kernel_size=(2, 3), data_format=data_format)(x)
     x = tf.keras.layers.BatchNormalization(axis)(x)
     x = tf.keras.layers.LeakyReLU(0.2)(x)
 
-    x = tf.keras.layers.Conv2DTranspose(filters=512, kernel_size=4, data_format=data_format)(x)
+    x = tf.keras.layers.Conv2DTranspose(filters=512, kernel_size=(2, 3),  data_format=data_format)(x)
     x = tf.keras.layers.BatchNormalization(axis)(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.Conv2DTranspose(filters=256,
+    x = tf.keras.layers.ReLU(0.2)(x)
+    x = tf.keras.layers.Conv2DTranspose(filters=256, kernel_size=4, strides=2, data_format=data_format)(x)
+    x = tf.keras.layers.BatchNormalization(axis)(x)
+    x = tf.keras.layers.ReLU(0.2)(x)
+    x = tf.keras.layers.ZeroPadding2D(((0, 0), (0, 1)), data_format=data_format)(x)
+    x = tf.keras.layers.Conv2DTranspose(filters=256, kernel_size=4, strides=2, data_format=data_format)(x)
+    x = tf.keras.layers.BatchNormalization(axis)(x)
+    x = tf.keras.layers.ReLU(0.2)(x)
+    x = tf.keras.layers.Conv2DTranspose(filters=128, kernel_size=4, strides=2, data_format=data_format)(x)
+    x = tf.keras.layers.BatchNormalization(axis)(x)
+    x = tf.keras.layers.ReLU(0.2)(x)
+    x = tf.keras.layers.ZeroPadding2D(((0, 1), (0, 1)), data_format=data_format)(x)
+    x = tf.keras.layers.Conv2DTranspose(filters=128, kernel_size=4, strides=2, data_format=data_format)(x)
+    x = tf.keras.layers.BatchNormalization(axis)(x)
+    x = tf.keras.layers.ReLU(0.2)(x)
+
+    x = tf.keras.layers.Conv2DTranspose(filters=1,
                                         kernel_size=4,
                                         strides=2,
                                         padding='same',
+                                        activation='tanh',
                                         data_format=data_format)(x)
-    x = tf.keras.layers.BatchNormalization(axis)(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.Conv2DTranspose(filters=128,
-                                        kernel_size=4,
-                                        strides=2,
-                                        padding='same',
-                                        data_format=data_format)(x)
-    x = tf.keras.layers.BatchNormalization(axis)(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.Conv2DTranspose(filters=64,
-                                        kernel_size=4,
-                                        strides=2,
-                                        padding='same',
-                                        data_format=data_format)(x)
-    x = tf.keras.layers.BatchNormalization(axis)(x)
-    x = tf.keras.layers.ReLU()(x)
-    
-    x = tf.keras.layers.Conv2DTranspose(filters=64,
-                                        kernel_size=4,
-                                        strides=2,
-                                        padding='same',
-                                        data_format=data_format)(x)
-    x = tf.keras.layers.BatchNormalization(axis)(x)
-    x = tf.keras.layers.ReLU()(x)
-    decoder_output = tf.keras.layers.Conv2DTranspose(filters=3,
-                                                     kernel_size=4,
-                                                     strides=2,
-                                                     padding='same',
-                                                     activation='tanh',
-                                                     data_format=data_format)(x)
-
+    decoder_output = tf.keras.layers.Cropping2D(((0, 0), (0, 1)), data_format=data_format)(x)
     autoencoder = tf.keras.Model(encoder_input, decoder_output, name='autoencoder')
 
     return autoencoder
 
 
-def build_discriminator(use_gpu, channels=3, height=32, width=32):
+def build_discriminator(use_gpu, channels=1, height=128, width=175):
     if use_gpu:
         data_format = 'channels_first'
         axis = 1
@@ -96,21 +79,25 @@ def build_discriminator(use_gpu, channels=3, height=32, width=32):
         discriminator_inputs = tf.keras.Input(shape=(height, width, channels))
 
     x = tf.keras.layers.Conv2D(filters=64,
-                               kernel_size=4,
-                               strides=2,
+                               kernel_size=3,
+                               strides=1,
                                padding='same',
                                data_format=data_format)(discriminator_inputs)
-    x = tf.keras.layers.LeakyReLU(0.2)(x)
-    #x = tf.keras.layers.Conv2D(filters=128, kernel_size=4, strides=2, padding='same', data_format=data_format)(x)
-    #x = tf.keras.layers.BatchNormalization(axis)(x)
-    #x = tf.keras.layers.LeakyReLU(0.2)(x)
-    x = tf.keras.layers.Conv2D(filters=256, kernel_size=4, strides=2, padding='same', data_format=data_format)(x)
     x = tf.keras.layers.BatchNormalization(axis)(x)
     x = tf.keras.layers.LeakyReLU(0.2)(x)
-    x = tf.keras.layers.Conv2D(filters=512, kernel_size=4, strides=2, padding='same', data_format=data_format)(x)
+    x = tf.keras.layers.MaxPool2D(2, data_format=data_format)(x)
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=1, padding='same', data_format=data_format)(x)
     x = tf.keras.layers.BatchNormalization(axis)(x)
     x = tf.keras.layers.LeakyReLU(0.2)(x)
-    x = tf.keras.layers.Conv2D(filters=512, kernel_size=4, data_format=data_format)(x)
+    x = tf.keras.layers.MaxPool2D(2, data_format=data_format)(x)
+    x = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=1, padding='same', data_format=data_format)(x)
+    x = tf.keras.layers.BatchNormalization(axis)(x)
+    x = tf.keras.layers.LeakyReLU(0.2)(x)
+    x = tf.keras.layers.MaxPool2D(2, data_format=data_format)(x)
+    x = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=1, padding='same', data_format=data_format)(x)
+    x = tf.keras.layers.BatchNormalization(axis)(x)
+    x = tf.keras.layers.LeakyReLU(0.2)(x)
+    x = tf.keras.layers.MaxPool2D(2, data_format=data_format)(x)
     x = tf.keras.layers.Flatten(data_format=data_format)(x)
     discriminator_output = tf.keras.layers.Dense(units=1, activation='sigmoid')(x)
 
