1de78f7897432c675b34d042ddeac88951d061f9
jblue1 <joblue@davidson.edu>
2019-11-14 22:34:46 -0500

    Changed data loading and normalization

--- CNN/train_CNN.py
+++ CNN/train_CNN.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 import CNN_model
 import click
 import os
-import load_data
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import date
@@ -21,22 +20,20 @@ def plot_loss(loss, val_loss, save_dir):
 
 
 @click.command()
-@click.argument('data_path', type=click.Path(exists=True, readable=True))
-@click.argument('lut_path', type=click.Path(exists=True, readable=True))
-@click.argument('indices_path', type=click.Path(exists=True, readable=True))
-@click.option('--num_events', default=-1)
+@click.argument('features_path', type=click.Path(exists=True, readable=True))
+@click.argument('targets_path', type=click.Path(exists=True, readable=True))
 @click.option('--batch_size', default=32)
 @click.option('--use_gpu/--no_gpu', default=False)
 @click.option('--epochs', default=50)
 @click.option('--lr', default=0.01, help='Learning rate for Adam optimizer')
 @click.option('--run_number', default=1, help='ith run of the day')
-def main(data_path, lut_path, indices_path, num_events, batch_size, use_gpu, epochs, lr, run_number):
+def main(features_path, targets_path, batch_size, use_gpu, epochs, lr, run_number):
     if not use_gpu:
         os.environ['CUDA_VISIBLE_DEVICES'] = ''
 
     today = str(date.today())
     run_number = '_' + str(run_number)
-    save_dir = './Run_' + today + run_number
+    save_dir = './Run_CNN_' + today + run_number
 
     if os.path.exists(save_dir):
         ans = input(
@@ -48,22 +45,30 @@ def main(data_path, lut_path, indices_path, num_events, batch_size, use_gpu, epo
     else:
         os.makedirs(save_dir)
     MSE = tf.keras.losses.MeanSquaredError(reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE)
-    model = CNN_model.build_covnet(use_gpu)
+    model = CNN_model.build_CNN(use_gpu)
     model.compile(optimizer=tf.keras.optimizers.Adam(lr), loss=MSE)
-    train_broken_data, train_data, val_broken_data, val_data = load_data.load_dataset(data_path,
-                                                                                      lut_path,
-                                                                                      indices_path,
-                                                                                      num_events)
+    print('Model is Compiled')
+    features = np.load(features_path)
+    print(np.max(features))
+    features /= np.max(features)
+    features = tf.expand_dims(features, -1)
+    print('Loaded features')
+    targets = np.load(targets_path)
+    targets /= np.max(targets)
+    print('Loaded Targets')
+
+
     checkpoint_path = os.path.join(save_dir, "checkpoints/cp-{epoch:04d}.ckpt")
+
     cp_callback = tf.keras.callbacks.ModelCheckpoint(
         checkpoint_path, verbose=1, save_weights_only=True,
         # Save weights, every 5-epochs.
         period=5)
-    history = model.fit(train_broken_data,
-                        train_data,
+    history = model.fit(features,
+                        targets,
                         batch_size=batch_size,
                         epochs=epochs,
-                        validation_data=(val_broken_data, val_data),
+                        validation_split=0.2,
                         callbacks=[cp_callback])
 
     loss = pd.Series(history.history['loss'])
