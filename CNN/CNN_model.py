73a27de7ccb8176c4d965062e3d2e6e22aed2011
jblue1 <joblue@davidson.edu>
2019-11-14 22:28:46 -0500

    Changed model architecture to predict only on biased pads

--- CNN/CNN_model.py
+++ CNN/CNN_model.py
@@ -6,42 +6,39 @@ z coordinate of each pad.
 import tensorflow as tf
 
 
-def build_convnet(use_gpu):
+def build_CNN(use_gpu):
     if use_gpu:
         data_format = 'channels_first'
         axis = 1
-        input = tf.keras.Input(shape=(1, 128, 175))
+        input = tf.keras.Input(shape=(1, 256, 128))
     else:
         data_format = 'channels_last'
         axis = -1
-        input = tf.keras.Input(shape=(128, 175, 1))
+        input = tf.keras.Input(shape=(256, 128, 1))
 
     x = tf.keras.layers.Conv2D(filters=64,
-                               kernel_size=4,
+                               kernel_size=3,
                                strides=1,
                                padding='same',
                                data_format=data_format)(input)
-    x = tf.keras.layers.MaxPooling2D((2,2), data_format=data_format)(x)
-    x = tf.keras.layers.BatchNormalization(axis=axis)(x)
-    x = tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=(1, 1), padding='same', data_format=data_format)(x)
+    x = tf.keras.layers.MaxPooling2D((3, 3), data_format=data_format)(x)
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=3, strides=1, data_format=data_format)(x)
     x = tf.keras.layers.MaxPooling2D((2, 2), data_format=data_format)(x)
-    x = tf.keras.layers.BatchNormalization(axis=axis)(x)
-    x = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=(1, 1), padding='same', data_format=data_format)(x)
+    x = tf.keras.layers.Conv2D(filters=64, kernel_size=3, strides=1, data_format=data_format)(x)
     x = tf.keras.layers.MaxPooling2D((2, 2), data_format=data_format)(x)
-    x = tf.keras.layers.BatchNormalization(axis=axis)(x)
-    x = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=(1, 1), padding='same', data_format=data_format)(x)
-    x = tf.keras.layers.MaxPooling2D((2, 2), data_format=data_format)(x)
-    x = tf.keras.layers.BatchNormalization(axis=axis)(x)
-    x = tf.keras.layers.Conv2D(filters=512, kernel_size=3, strides=(1, 1), padding='same', data_format=data_format)(x)
-    x = tf.keras.layers.MaxPooling2D((2, 2), data_format=data_format)(x)
-    x = tf.keras.layers.BatchNormalization(axis=axis)(x)
-    output = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dropout(0.3)(x)
+    x = tf.keras.layers.Dense(5000)(x)
+    x = tf.keras.layers.Dropout(0.3)(x)
+    x = tf.keras.layers.Dense(5000)(x)
+    x = tf.keras.layers.Dropout(0.3)(x)
+    output = tf.keras.layers.Dense(4608)(x)
     cnn = tf.keras.Model(input, output, name='cnn')
     return cnn
 
 
 def main():
-    model = build_convnet(False)
+    model = build_CNN(False)
     model.summary()
 
 
