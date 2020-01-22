cd1184d4df8b7cd125b96960cf9fd8b852108fd5
jblue1 <joblue@davidson.edu>
2020-01-22 13:01:24 -0500

    Changed last layer activation to sigmoid for classification

--- FCNN/FCNN_model.py
+++ FCNN/FCNN_model.py
@@ -16,9 +16,15 @@ def build_model():
     model.add(tf.keras.layers.Dropout(0.3))
     model.add(tf.keras.layers.Dense(5000, activation='relu'))
     model.add(tf.keras.layers.Dropout(0.3))
-    model.add(tf.keras.layers.Dense(4608))
+    model.add(tf.keras.layers.Dense(4608, activation='sigmoid'))
 
     return model
 
 
+def main():
+    model = build_model()
+    model.summary()
 
+
+if __name__ == '__main__':
+    main()
